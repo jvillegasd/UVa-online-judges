9a000f4fbfeff13c93a5f66ad1475041bff0e206
C9-LinkRs <linkrsfan@gmail.com>
2017-11-25 22:41:40 -0500

    Finished

---
+++ AOAPC I Beginning Algorithm Contests (Rujia Liu)/Volume 1/Sorting-Searching/UVA10420.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main(){
+    int n;
+    map<string, int> db;  //A map sorts its key internally
+    string line, country;
+    cin >> n;
+    getline(cin, line);
+    while(n-- > 0){
+        cin >> country;
+        getline(cin, line);
+        db[country] +=1;
+    }
+    for(auto iterator = db.begin(); iterator != db.end(); iterator++){
+        cout << iterator->first << " " << iterator->second << endl;
+    }
+}
