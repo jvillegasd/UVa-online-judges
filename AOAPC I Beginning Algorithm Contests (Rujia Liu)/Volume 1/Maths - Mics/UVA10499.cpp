39abdbd91c783d75e4a8882a5f882eaa5560c612
C9-LinkRs <linkrsfan@gmail.com>
2017-12-16 00:35:08 -0500

    UVA10499 commented

--- AOAPC I Beginning Algorithm Contests (Rujia Liu)/Volume 1/Maths - Mics/UVA10499.cpp
+++ AOAPC I Beginning Algorithm Contests (Rujia Liu)/Volume 1/Maths - Mics/UVA10499.cpp
@@ -11,7 +11,9 @@ int main(){
     return 0;
 }
 /*
-    Al cortar la esfera se nos genera una semiesfera, nos piden que calculemos la ganancia por area superficial, asi que no consideraremos la semiesfera, sino, los dos semicirculos, dicho esto se divide el area del circulo con la de la 
-    esfera inicial: pi*r^2/4*pi*r^2 = 1/4 = 25%, es decir, por cada parte se va generando una ganacia del 25%, digo esto debido a que la parte base es el area de la esfera, de aqui se puede generar una funcion de coste:
-    C(x) = 4*pi*r + x*(4*pi*0.25)*r , para: x > 1 y C(x) = 4*pi para x = 1.
+    Al cortar la esfera se nos genera una semiesfera, nos piden que calculemos la ganancia por area superficial, 
+    asi que no consideraremos la semiesfera, sino, los dos semicirculos, dicho esto se divide el area del circulo 
+    con la de la  esfera inicial: pi*r^2/4*pi*r^2 = 1/4 = 25%, es decir, por cada parte se va generando una 
+    ganacia del 25%, digo esto debido a que la parte base es el area de la esfera, de aqui se puede generar una 
+    funcion de coste: C(x) = 4*pi*r + x*(4*pi*0.25)*r , para: x > 1 y C(x) = 4*pi para x = 1.
 */
