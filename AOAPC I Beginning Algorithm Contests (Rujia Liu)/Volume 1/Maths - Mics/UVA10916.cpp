771a37d0112940606ce9e378e2fa188118c84b43
C9-LinkRs <linkrsfan@gmail.com>
2018-01-05 22:24:39 -0500

    UVA10916 added

--- AOAPC I Beginning Algorithm Contests (Rujia Liu)/Volume 1/Maths - Mics/UVA10916.cpp
+++ AOAPC I Beginning Algorithm Contests (Rujia Liu)/Volume 1/Maths - Mics/UVA10916.cpp
@@ -24,5 +24,5 @@ int main(){
     El numero mas grande es 2^k donde "k" son los bits del pc, para saber el numero mas grande que puede tener
     la pc (del problema): numM = 2^(2+[(year - 1960)/10]). Teniendo en cuenta que 2^k > n! (solo piden que sea uInt),
     hacemos algo de algebra quedaria k > log2(n!) y si re-expresamos el factorial: k > log2(n)+log2(n-1)+...+log2(1),
-    como sabemos que 2^k > k, entonces 2^k > log2(n)+log2(n-1)+...+log2(1).
+    como sabemos que 2^k > k, entonces 2^k > log2(n)+log2(n-1)+...+log2(1) (Se hace esto para hacer menos calculo).
 */
