2cc8d18ea7378242fa29eff2d00cb3386c1b86b7
C9LinkRs <linkrsfan%40gmail.com>
2018-06-24 17:18:57 -0500

    UVA10970 completed

--- AOAPC I Beginning Algorithm Contests (Rujia Liu)/Volume 1/Maths - Mics/UVA10970.cpp
+++ AOAPC I Beginning Algorithm Contests (Rujia Liu)/Volume 1/Maths - Mics/UVA10970.cpp
@@ -3,9 +3,14 @@
 #include <string.h>
 #include <map>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 int main(){
-
+    long long int m,n;
+    while(scanf("%lld %lld", &m, &n) != EOF){
+        printf("%d\n", m*n-1);
+    }
+    return 0;
 }
