4fec09f98752e3663bc863ff9808c5fdef6b4e8d
Johnny Villegas <linkrsfan@gmail.com>
2019-08-03 01:07:42 -0500

    Add files via upload

---
+++ UVa 455.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define pf push_front
+#define pob pop_back
+#define fp first
+#define sp second
+#define mp make_pair
+#define ins insert
+#define uEdge(u, v) g[u].pb(v), g[v].pb(u)
+#define uwEdge(u, v, w) g[u].pb({v, w}), g[v].pb({u, w})
+#define dEdge(u, v) g[u].pb(v)
+#define dwEdge(u, v, w) g[u].pb({v, w})
+#define BOOST ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define minHeap int, vector<int>, greater<int>
+#define MOD 1000000007
+
+using namespace std;
+typedef long long int lli;
+typedef pair<int, int> pii;
+lli nl, arr[0];
+vector<int> g[0];
+
+lli gcd(lli a, lli b){if(b == 0) return a;a%=b;return gcd(b, a);}
+lli lcm(lli a, lli b){return (a*b)/gcd(a, b);}
+int bs(lli sum){int l=0,r=nl-1,mid;while(l<=r){mid=(l+r)/2;if(arr[mid]==sum)return mid;if(arr[mid]<sum)l=mid+1;else r=mid-1;}return -1;}
+lli modPow(lli a, lli b){a%=MOD;lli ans=1;while(b > 0){if(b & 1) ans=ans*a%MOD;a=a*a%MOD;b>>=1;}return ans;}
+
+vector<int> z_function(string s){
+    int n = (int) s.length();
+    vector<int> z(n);
+    for (int i = 1, l = 0, r = 0; i < n; ++i) {
+        if (i <= r)
+            z[i] = min (r - i + 1, z[i - l]);
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+            ++z[i];
+        if (i + z[i] - 1 > r)
+            l = i, r = i + z[i] - 1;
+    }
+    return z;
+}
+
+int string_compression(string s){
+    vector<int> z=z_function(s);
+    int n=(int)s.length(), ans=0;
+    for(int i = 1; i < z.size(); i++){
+        if(n%i == 0 && i+z[i] == n){
+            ans=i;
+            break;
+        }
+    }
+    return (ans == 0)?n:ans;
+}
+
+int main(){
+    int tc;
+    string s;
+    cin >> tc;
+    getline(cin, s);
+    for(int i = 0; i < tc; i++){
+        getline(cin, s);
+        getline(cin, s);
+        if(i) printf("\n");
+        printf("%d\n", string_compression(s));
+    }
+    return 0;
+}
